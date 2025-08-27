615f1323213422d3e58a29927c339b824a0ed152
mmgriffi55 <michaelmgriffin82@gmail.com>
2025-08-27 14:04:27 +0100

    moved file

---
+++ job-dsl/nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker example') {
+    scm {
+        git('https://github.com/mmgriffi/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('michaelmgriffin82@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('mmgriffi/docker-nodejs-demodsl')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
